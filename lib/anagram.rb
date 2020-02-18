2b9f22d601cde762d1035968319c4a358d8ae8ba
Stacy Johns <stacykjohns@icloud.com>
2020-02-18 20:12:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,13 @@
 class Anagram
 
   attr_reader :words
-  
+
   def initialize(words)
     @words = words
   end
 
-  def match(array)
-    array.find_all do |word|
+  def match(array_of_words)
+    array_of_words.find_all do |word|
       if word.split("").sort == self.words.split("").sort
         word
       end
