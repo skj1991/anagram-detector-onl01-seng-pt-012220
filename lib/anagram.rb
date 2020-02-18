02c66d6386040d6cacf71bc53ce2c97e91077d80
Stacy Johns <stacykjohns@icloud.com>
2020-02-18 20:09:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,10 +5,10 @@ class Anagram
   end
 
   def match(array)
-    find_this.find_all do |word| 
+    find_this.find_all do |word|
 
-      if word.split("").sort == self.array_words.split("").sort 
-        word 
+      if word.split("").sort == self.array_words.split("").sort
+        word
       end
   end
 
