56016481f4de492982c61e0b90f20bbaceb64794
Stacy Johns <stacykjohns@icloud.com>
2020-02-18 20:12:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(array_of_words)
+    array_of_words.find_all do |word|
+      if word.split("").sort == self.words.split("").sort
+        word
+      end
+    end
+  end
+
+end
